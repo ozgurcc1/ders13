0074daf5a6def04d366ef22cd437bc19cdf150a1
Ozgur Celik <ozgurce869@gmail.com>
2022-08-12 21:42:25 +0300

    Event örnekleri gösterildi.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
+import {useState} from "react";
 
 function App() {
 
-  
+  const [bilgi, bilgiGuncelle] = useState("");
+
+  const fonksiyon1 = ()=>{
+    console.log("Buton tıklandı!");
+  }
+
+  const fonksiyon2 = (e)=>{
+    bilgiGuncelle(e.target.value);
+  } 
   return (
    <>
-   
-   <p>Merhaba</p>
+   <input onChange={fonksiyon2} type="text"/>
+   <button onClick={fonksiyon1}>Uyarı</button>
+   <p>{bilgi.length > 10 ? "10 karakteri geçemez" : ""}</p>
    </>
   );
 }
