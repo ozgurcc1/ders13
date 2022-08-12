95e98e5da3cf999d6f41e69f7bf369ddb1fbc430
Ozgur Celik <ozgurce869@gmail.com>
2022-08-12 21:40:51 +0300

    yeniden güncellendi.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import {useState} from "react";
+
 
 function App() {
 
-  const [bilgi, bilgiGuncelle] = useState("");
-
-  const fonksiyon1 = ()=>{
-    console.log("Buton tıklandı!");
-  }
-
-  const fonksiyon2 = (e)=>{
-    bilgiGuncelle(e.target.value);
-  } 
+  
   return (
    <>
-   <input onChange={fonksiyon2} type="text"/>
-   <button onClick={fonksiyon1}>Uyarı</button>
-   <p>{bilgi.length > 10 ? "10 karakteri geçemez" : ""}</p>
+   
+   <p>Merhaba</p>
    </>
   );
 }
