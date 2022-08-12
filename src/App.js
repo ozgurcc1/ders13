5fa4789eee9fcdc527f22e6853a98672c96f47bc
Ozgur Celik <ozgurce869@gmail.com>
2022-08-12 21:28:38 +0300

    Logo ve css import silindi.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [bilgi, bilgiGuncelle] = useState("");
+
+  const fonksiyon1 = ()=>{
+    console.log("Buton tıklandı!");
+  }
+
+  const fonksiyon2 = (e)=>{
+    bilgiGuncelle(e.target.value);
+  } 
   return (
-   <p>Merhaba</p>
+   <>
+   <input onChange={fonksiyon2} type="text"/>
+   <button onClick={fonksiyon1}>Uyarı</button>
+   <p>{bilgi.length > 10 ? "10 karakteri geçemez" : ""}</p>
+   </>
   );
 }
 
