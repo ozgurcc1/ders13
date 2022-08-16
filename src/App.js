bee4e4679946c64716ceb1ff069cfb8115c903d6
Ozgur Celik <ozgurce869@gmail.com>
2022-08-16 21:30:36 +0300

    useRef basit örnek

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
+import {useEffect, useRef} from "react";
 
 function App() {
+  const eleman1 = useRef();
+
+ useEffect(()=>{
+  eleman1.current.addEventListener("click", ()=>{
+    alert("Merhaba");
+  });
+  console.log(eleman1.current.innerHTML);
+ })
 
   
   return (
    <>
    
-   <p>Merhaba</p>
+   <p ref={eleman1}>Merhaba!</p>
    </>
   );
 }
